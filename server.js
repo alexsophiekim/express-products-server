999a08e9428f005277a1cd52950e0b5abfdf757c
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-08-17 15:27:56 +1200

    min&max price

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.get('/all',function(req,res){
   res.json(data);
 });
 
-app.get('/all/in_stock=:in_stock', function(req,res){
+// URL will be http://localhost:3000/all/in_stock=false
+app.get('/in_stock=:in_stock', function(req,res){
   const stock = req.params.in_stock;
   if ((stock === "true") || (stock === "false")) {
     let filteredData =[];
@@ -26,20 +27,70 @@ app.get('/all/in_stock=:in_stock', function(req,res){
   } else {
     res.send('Invalid')
   }
-
 });
 
-app.get('/all/min_price=:value', function(req,res){
+const min_price = app.get('/min_price=:product_price', function(req,res){
   const price = req.params.product_price;
-  let filteredData = [];
-  for (var i = 0; i < data.length; i++) {
-    if (price === data[i].product_price.toString()) {
-      filteredData.push(data[i]);
+  if ((price > 0) && (price <= 10)) {
+    let filteredData = [];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].product_price.toString()===price) {
+        filteredData.push(data[i]);
+      }
     }
+    res.send(filteredData);
+  } else {
+    res.send('Invalid')
   }
-  res.send(Math.min(`${filteredData}`));
+  // res.send(Math.min(`${filteredData}`));
+});
 
+const max_price = app.get('/max_price=:product_price',function(req,res){
+  const price = req.params.product_price;
+  if ((price == 100)) {
+    let filteredData = [];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].product_price.toString()===price) {
+        filteredData.push(data[i]);
+      }
+    }
+    res.send(filteredData);
+  } else {
+    res.send('Invalid')
+  }
 })
+
+app.get('/min_price=:value/max_price=:value',function(req,res){
+  const price = req.params.product_price;
+  if (min_price && max_price) {
+    let filteredData=[];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].product_price.toString()===price) {
+        filteredData.push(data[i]);
+      }
+    }
+    res.send(filteredData);
+  } else {
+    res.send('Invalid')
+  }
+})
+//
+// app.get('min_price=:value/max_price=:value', function(req,res){
+//   const price = req.params.product_price;
+//   const min_price = ((price>0) && (price<=10));
+//   const max_price = (price = 100)
+//   if (min_price || max_price) {
+//     let filteredData=[];
+//     for (var i = 0; i < data.length; i++) {
+//       if (data[i].product_price.toString()===price) {
+//         filteredData.push(data[i]);
+//       }
+//     }
+//     res.send(filteredData);
+//   } else {
+//     res.send('Invalid')
+//   }
+// })
 
 
 app.listen(port, ()=> console.log(`application is running on port ${port}`));
